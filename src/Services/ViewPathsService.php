d6c530773e7b93d98cbbc9d55c32316cd4f93c0e
Iurie Malai <iurie.malai@gmail.com>
2025-05-08 11:33:11 +0200

    Added setLocale() method

--- src/Services/ViewPathsService.php
+++ src/Services/ViewPathsService.php
@@ -5,7 +5,6 @@ namespace IurieMalai\ViewPaths\Services;
 use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Log\LogManager;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 use Statamic\Facades\Cascade;
